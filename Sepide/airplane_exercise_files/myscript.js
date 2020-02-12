ac72d1365278a31106802800cbd440a7032f6dd0
Sepide <Sepide.niki@gmail.com>
2020-02-12 15:01:41 +0100

    boomb

--- Sepide/airplane_exercise_files/myscript.js
+++ Sepide/airplane_exercise_files/myscript.js
@@ -1,35 +1,40 @@
 $(document).ready(function() {
-// 	$("#moveme").css("left",.draggable();
-// 	$("#moveme").mousemove(function(e){
-//       $('#airplain').css({'top': e.clientY - 20, 'left': e.clientX - 20});
-// });
-	// body...
 	var y;
+	var X;
 	 $(document).mousemove(function(a) { 
-
-              //console.log("X= " + a.pageX); 
-             // console.log("Y= " + a.pageY);
-               y = a.pageY;
-           
-              $("#moveme").css({"left": a.pageX-100,"top": a.pageY-100});
+	 	//console.log(y +"Y =");
+	 	console.log(a.pageY);
+               y = a.pageY; 
+               X = a.pageX;          
+              
+              if(y>160 && y<450 && X>124 && X<1070)
+              {
+              	 $("#moveme").css({"left": a.pageX-100,"top": a.pageY-100});
+              }
               
       }); 
 	 $("#moveme").click(function() {
+	$("#RaketDiv").show();
+	 	// console.log(y +"Y =");
 
-	 	console.log(y +"Y =");
-         var boundY= window.innerHeight;
-         console.log(boundY +"boundY -");
-         var fall=boundY-y;
-          	console.log(fall + "result");
-	 	 $(document).off( "mousemove");
-	 	 $(this).animate({
-			top: fall},
-			500, function(){
-				$(this).fadeOut("slow").fadeIn("slow");
+   //       //var boundY= window.innerHeight;
+   //       var boundY=$("#divMain").height();
+   //       console.log(boundY +"boundY -");
+   //       var fall=boundY-y;
+   //        	console.log(fall + "result");
+	 	//  
+	 	$(document).off( "mousemove");
+
+	 	 $("#RaketDiv").animate({
+			top: y,
+			left: X},
+			1500, function(){
+				
+             $("#RaketDiv").hide();
+             $("#moveme img").attr("src","boom.gif").css("border-radius","50%");
+             
 			});
-		
-	 	// $("#moveme").fadeOut("slow").fadeIn("slow");
-	 	// body...
+	
 	 });
 
 });
