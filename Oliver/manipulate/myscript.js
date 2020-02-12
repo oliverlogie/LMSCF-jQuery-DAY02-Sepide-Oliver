6a8c1a82a33789fd711351c7c24f14067aeb78cd
oliverlogie <oliver.s.kraus@gmail.com>
2020-02-12 14:57:51 +0100

    advanced exercise

--- Oliver/manipulate/myscript.js
+++ Oliver/manipulate/myscript.js
@@ -1,3 +1,28 @@
 $("img[src*=phone]").parent().css("background", "green");
 $("img[src*=tablet]").parent().css("background", "blue");
 $("img[src*=notebook]").parent().css("background", "red");
+
+$(".notebook").click(function(){
+	$("img[src*=phone]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=tablet]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=notebook]").parent().parent().parent().parent().parent().css("display", "block");
+});
+
+$(".tablet").click(function(){
+	$("img[src*=phone]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=notebook]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=tablet]").parent().parent().parent().parent().parent().css("display", "block");
+});
+
+$(".phones").click(function(){
+	$("img[src*=tablet]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=notebook]").parent().parent().parent().parent().parent().css("display", "none");
+	$("img[src*=phone]").parent().parent().parent().parent().parent().css("display", "block");
+
+});
+
+$(".all").click(function(){
+	$("img[src*=tablet]").parent().parent().parent().parent().parent().css("display", "block");
+	$("img[src*=notebook]").parent().parent().parent().parent().parent().css("display", "block");
+	$("img[src*=phone]").parent().parent().parent().parent().parent().css("display", "block");
+});
--- Oliver/plane/myscript.js
+++ Oliver/plane/myscript.js
@@ -4,20 +4,32 @@ $(document).ready(function() {
 //       $('#airplain').css({'top': e.clientY - 20, 'left': e.clientX - 20});
 // });
 	// body...
-	 $(document).mousemove(function(event) {
-              console.log("X= " + event.pageX);
-              console.log("Y= " + event.pageY);
-              $("#moveme").css({"left": event.pageX-100,"top": event.pageY-100});
-      });
+	var y;
+	 $(document).mousemove(function(a) { 
+
+              //console.log("X= " + a.pageX); 
+             // console.log("Y= " + a.pageY);
+               y = a.pageY;
+           
+              $("#moveme").css({"left": a.pageX-100,"top": a.pageY-100});
+              
+      }); 
 	 $("#moveme").click(function() {
-	 	var a = 
+
+	 	console.log(y +"Y =");
+         var boundY= window.innerHeight;
+         console.log(boundY +"boundY -");
+         var fall=boundY-y;
+          	console.log(fall + "result");
 	 	 $(document).off( "mousemove");
 	 	 $(this).animate({
-			top: '+=500'},
+			top: fall},
 			500, function(){
 				$(this).fadeOut("slow").fadeIn("slow");
 			});
+		
 	 	// $("#moveme").fadeOut("slow").fadeIn("slow");
 	 	// body...
 	 });
+
 });
